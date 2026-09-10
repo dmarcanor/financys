b5cc80145810815313db05c0b4e9ebab0822e570
Daniel Marcano <dmarcanor2@gmail.com>
2026-09-10 16:48:56 -0400

    update .env.examle

---
+++ src/Financys/Auth/Infrastructure/LaravelAuth.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Financys\Auth\Infrastructure;
+
+use Auth;
+use Financys\Auth\Domain\Authentication;
+use Financys\Auth\Domain\AuthenticationRepository;
+
+class LaravelAuth implements AuthenticationRepository
+{
+    public function authenticate(string $email, string $password): ?Authentication
+    {
+        $token = Auth::attempt([
+            'email' => $email,
+            'password' => $password
+        ]);
+
+        if (!$token) {
+            return null;
+        }
+
+        return new Authentication(
+            
+        );
+    }
+}
