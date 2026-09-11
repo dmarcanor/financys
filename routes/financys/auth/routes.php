ca2ea2cb6a42e15abb826de0fd3556d135da631a
Daniel Marcano <dmarcanor2@gmail.com>
2026-09-10 22:17:06 -0400

    chore: import Route facade in auth routes

--- routes/financys/auth/routes.php
+++ routes/financys/auth/routes.php
@@ -3,6 +3,7 @@
 declare(strict_types = 1);
 
 use App\Http\Controllers\Auth\LoginPostController;
+use Illuminate\Support\Facades\Route;
 
 // non-authenticated routes
 Route::post('login', [LoginPostController::class, '__invoke']);
