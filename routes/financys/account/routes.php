3e419071adb04c1912466b76d6e58c3e2f501182
Daniel Marcano <dmarcanor2@gmail.com>
2026-09-10 22:17:05 -0400

    fix(auth): apply jwt.auth middleware to account route

--- routes/financys/account/routes.php
+++ routes/financys/account/routes.php
@@ -3,7 +3,8 @@
 declare(strict_types = 1);
 
 use App\Http\Controllers\Account\AccountPostController;
+use Illuminate\Support\Facades\Route;
 
-Route::prefix('')->group(function () {
+Route::middleware('jwt.auth')->prefix('')->group(function () {
     Route::post('account', [AccountPostController::class, '__invoke']);
-})->middleware('jwt.auth');
+});
