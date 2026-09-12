dfc821fa74a3df852e7242383921679057319b7f
Daniel Marcano <dmarcanor2@gmail.com>
2026-09-11 20:08:27 -0400

    feat(account): add find by id to repository

--- src/Financys/Account/Domain/AccountRepository.php
+++ src/Financys/Account/Domain/AccountRepository.php
@@ -6,4 +6,6 @@ namespace Financys\Account\Domain;
 
 interface AccountRepository {
     public function create(Account $account): void;
+
+    public function find(string $id): ?Account;
 }
--- src/Financys/Account/Infrastructure/Postgre/AccountPostgreRepository.php
+++ src/Financys/Account/Infrastructure/Postgre/AccountPostgreRepository.php
@@ -1,17 +1,20 @@
 <?php
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 namespace Financys\Account\Infrastructure\Postgre;
 
-use DB;
 use Financys\Account\Domain\Account;
+use Financys\Account\Domain\AccountBalance;
+use Financys\Account\Domain\AccountCode;
+use Financys\Account\Domain\AccountName;
 use Financys\Account\Domain\AccountRepository;
+use Illuminate\Support\Facades\DB;
+use Shared\Domain\Symbols;
+use Shared\Domain\Uuid;
 
 class AccountPostgreRepository implements AccountRepository
 {
-
-
     public function create(Account $account): void
     {
         DB::table('accounts')
@@ -23,7 +26,29 @@ class AccountPostgreRepository implements AccountRepository
                 'balance' => $account->balance()->amount(),
                 'currency' => $account->balance()->symbol(),
                 'created_at' => now(),
-                'updated_at'=> now(),
+                'updated_at' => now(),
             ]);
+    }
+
+    public function find(string $id): ?Account
+    {
+        $account = DB::table('accounts')
+            ->where('id', $id)
+            ->first();
+
+        if ($account === null) {
+            return null;
+        }
+
+        return new Account(
+            new Uuid($account->id),
+            new Uuid($account->user_id),
+            new AccountCode($account->code),
+            new AccountName($account->name),
+            new AccountBalance(
+                Symbols::from($account->currency),
+                $account->balance,
+            )
+        );
     }
 }
