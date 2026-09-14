b773bfae1fc26459fbd220428298deeb071166c3
Daniel Marcano <dmarcanor2@gmail.com>
2026-09-14 18:54:47 -0400

    test(account): cover account balance rules

--- tests/Unit/Src/Account/Domain/AccountBalanceTest.php
+++ tests/Unit/Src/Account/Domain/AccountBalanceTest.php
@@ -11,37 +11,26 @@ use Tests\TestCase;
 
 class AccountBalanceTest extends TestCase
 {
-    public function test_it_should_pad_whole_number_to_eight_decimals(): void
+    public function test_it_should_create_balance_with_symbol_and_normalized_amount(): void
     {
         $balance = AccountBalance::create(Symbols::USD, '5');
 
-        expect($balance->amount())->toBe('5.00000000');
+        expect($balance->amount())->toBe('5.00000000')
+            ->and($balance->symbol())->toBe('usd');
     }
 
-    public function test_it_should_pad_short_fraction_to_eight_decimals(): void
+    public function test_it_should_accept_bs_symbol(): void
     {
-        $balance = AccountBalance::create(Symbols::USD, '5.1');
+        $balance = AccountBalance::create(Symbols::BS, '10.5');
 
-        expect($balance->amount())->toBe('5.10000000');
-    }
-
-    public function test_it_should_truncate_more_than_eight_decimals(): void
-    {
-        $balance = AccountBalance::create(Symbols::USD, '5.123456789');
-
-        expect($balance->amount())->toBe('5.12345678');
-    }
-
-    public function test_it_should_clean_leading_zeros(): void
-    {
-        $balance = AccountBalance::create(Symbols::USD, '0005');
-
-        expect($balance->amount())->toBe('5.00000000');
+        expect($balance->amount())->toBe('10.50000000')
+            ->and($balance->symbol())->toBe('bs');
     }
 
     public function test_it_should_throw_negative_currency_when_balance_is_negative(): void
     {
         $this->expectException(NegativeCurrency::class);
+        $this->expectExceptionMessage("The account balance -100.000000 usd can't be negative");
 
         AccountBalance::create(Symbols::USD, '-100');
     }
@@ -51,5 +40,12 @@ class AccountBalanceTest extends TestCase
         $balance = AccountBalance::create(Symbols::USD, '-0');
 
         expect($balance->amount())->toBe('0.00000000');
+    }
+
+    public function test_it_should_normalize_amount_on_create(): void
+    {
+        $balance = AccountBalance::create(Symbols::USD, '5.123456789');
+
+        expect($balance->amount())->toBe('5.12345678');
     }
 }
