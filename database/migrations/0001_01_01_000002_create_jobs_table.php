2dcbbbb16efffe7a4682dc27b3a46eb1a63638b7
Daniel Marcano <dmarcanor2@gmail.com>
2026-09-18 13:16:43 -0400

    fix(migration): normalize failed_jobs schema and add index

--- database/migrations/0001_01_01_000002_create_jobs_table.php
+++ database/migrations/0001_01_01_000002_create_jobs_table.php
@@ -35,12 +35,15 @@ return new class extends Migration
         });
 
         Schema::create('failed_jobs', function (Blueprint $table) {
-            $table->uuid('id')->primary();
-            $table->text('connection');
-            $table->text('queue');
+            $table->id();
+            $table->string('uuid')->unique();
+            $table->string('connection');
+            $table->string('queue');
             $table->longText('payload');
             $table->longText('exception');
             $table->timestamp('failed_at')->useCurrent();
+
+            $table->index(['connection', 'queue', 'failed_at']);
         });
     }
 
