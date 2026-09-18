fabbbd265ea9b3a81b69b628e15f023fefc485a3
Daniel Marcano <dmarcanor2@gmail.com>
2026-09-18 13:16:51 -0400

    chore(listener): add debug instrumentation to event handler

--- app/Listeners/SaveHistoryOnAccountCreated.php
+++ app/Listeners/SaveHistoryOnAccountCreated.php
@@ -5,6 +5,7 @@ namespace App\Listeners;
 use App\Events\AccountCreated;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Events\attributes\Listener;
+use Illuminate\Support\Facades\Log;
 
 #[Listener(AccountCreated::class)]
 class SaveHistoryOnAccountCreated implements ShouldQueue
@@ -22,17 +23,19 @@ class SaveHistoryOnAccountCreated implements ShouldQueue
      */
     public function handle(AccountCreated $event): void
     {
+        dd($event);
+        Log::alert('SaveHistoryOnAccountCreated', json_encode($event));
         // create a file
-        $filePath = storage_path('app/history.txt');
-        $fileContent = sprintf(
-            "Account created: %s, User ID: %s, Code: %s, Name: %s, Balance: %s, Currency: %s\n",
-            $event->account->id,
-            $event->account->userId,
-            $event->account->code,
-            $event->account->name,
-            $event->account->balance,
-            $event->account->currency
-        );
-        file_put_contents($filePath, $fileContent, FILE_APPEND);
+        // $filePath = storage_path('app/history.txt');
+        // $fileContent = sprintf(
+        //     "Account created: %s, User ID: %s, Code: %s, Name: %s, Balance: %s, Currency: %s\n",
+        //     $event->account->id,
+        //     $event->account->userId,
+        //     $event->account->code,
+        //     $event->account->name,
+        //     $event->account->balance,
+        //     $event->account->currency
+        // );
+        // file_put_contents($filePath, $fileContent, FILE_APPEND);
     }
 }
