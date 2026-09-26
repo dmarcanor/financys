33f3c68c99b573e70c0587fef3dc3e755ff930e1
Daniel <daniel@example.com>
2026-09-25 21:56:49 -0400

    refactor(auth): throw FailedAuthenticationException on null authentication

--- src/Financys/Auth/Application/Authenticator.php
+++ src/Financys/Auth/Application/Authenticator.php
@@ -5,20 +5,24 @@ declare(strict_types=1);
 namespace Financys\Auth\Application;
 
 use Financys\Auth\Domain\AuthenticationRepository;
+use Financys\Auth\Domain\FailedAuthenticationException;
 
 class Authenticator
 {
-    public function __construct(private AuthenticationRepository $authRepository) {}
+    public function __construct(private AuthenticationRepository $authenticationRepository) {}
 
     public function __invoke(AuthenticatorRequest $request): AuthenticatorResponse
     {
-        $authentication = $this->authRepository->authenticate(
+        $authentication = $this->authenticationRepository->authenticate(
             $request->email,
             $request->password
         );
 
+        if ($authentication === null) {
+            throw new FailedAuthenticationException();
+        }
+
         return new AuthenticatorResponse(
-            $authentication->userId->value(),
             $authentication->token,
             $authentication->type,
             $authentication->expiresAt->format('Y-m-d H:i:s')
