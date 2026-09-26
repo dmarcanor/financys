543ba3e4fa0a0a9b02771c6fc5f7f2cb1fbdbdaf
Daniel <daniel@example.com>
2026-09-25 21:56:46 -0400

    feat(auth): implement LaravelAuth with token, type and expiration

--- src/Financys/Auth/Infrastructure/LaravelAuth.php
+++ src/Financys/Auth/Infrastructure/LaravelAuth.php
@@ -4,12 +4,16 @@ declare(strict_types=1);
 
 namespace Financys\Auth\Infrastructure;
 
-use Auth;
+use DateInterval;
+use DateTimeImmutable;
 use Financys\Auth\Domain\Authentication;
 use Financys\Auth\Domain\AuthenticationRepository;
+use Illuminate\Support\Facades\Auth;
 
 class LaravelAuth implements AuthenticationRepository
 {
+    public const string TOKEN_TYPE = 'bearer';
+
     public function authenticate(string $email, string $password): ?Authentication
     {
         $token = Auth::attempt([
@@ -21,6 +25,12 @@ class LaravelAuth implements AuthenticationRepository
             return null;
         }
 
-        return new Authentication;
+        $expirationTimeInMinutes = auth()->factory()->getTTL();
+
+        return new Authentication(
+            $token,
+            self::TOKEN_TYPE,
+            (new DateTimeImmutable())->add(new DateInterval("PT{$expirationTimeInMinutes}M"))
+        );
     }
 }
